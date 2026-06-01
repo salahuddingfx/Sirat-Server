6fe8f76d02396625e71dd5ede7b31f40578f5773
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-01 13:10:02 +0600

    feat: implement MongoDB connection configuration using mongoose

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("  \x1b[1;33m» DATABASE:\x1b[0m   \x1b[1;32mConnected & Synced\x1b[0m");
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
