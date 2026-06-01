008b2423d103437ef41981fb16c0532659cc6d76
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-01 21:05:49 +0600

    refactor: optimize server request handling and response processing in server.js

--- server.js
+++ server.js
@@ -46,7 +46,10 @@ app.use((req, res, next) => {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(",") : ["http://localhost:3000", "http://localhost:5173"],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
