14e4cf051435b9389d11d456d51a5c01c59f0786
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-01 12:40:07 +0600

    feat: add settings routes for retrieving and updating configurations with admin authorization

--- routes/settings.routes.js
+++ routes/settings.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const settingsController = require("../controllers/settings.controller");
+const { protect, admin } = require("../middleware/auth.middleware");
 
 router.get("/", settingsController.getSettings);
-router.put("/", settingsController.updateSettings);
+router.put("/", protect, admin, settingsController.updateSettings);
 
 module.exports = router;
