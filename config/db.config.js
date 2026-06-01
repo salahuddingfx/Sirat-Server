8cb30bbbfac218a208850471e94372c258ea3484
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-01 13:13:15 +0600

    feat: initialize database connection module and environment configuration utility

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const env = require("./env.config");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(env.mongoUri);
     console.log("  \x1b[1;33m» DATABASE:\x1b[0m   \x1b[1;32mConnected & Synced\x1b[0m");
   } catch (error) {
     console.error(`Error: ${error.message}`);
