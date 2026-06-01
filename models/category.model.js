75f54113b0b6b7f21c4b74f626b9bf6a2ef354e6
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-01 13:42:04 +0600

    feat: add category model and routes with image upload support

---
+++ models/category.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    image: { type: String, required: true } // Cloudinary image URL
+  },
+  { timestamps: true }
+);
+
+categorySchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+categorySchema.set("toJSON", { virtuals: true });
+categorySchema.set("toObject", { virtuals: true });
+
+module.exports = mongoose.model("Category", categorySchema);
