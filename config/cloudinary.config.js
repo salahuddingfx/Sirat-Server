af108294cce26dedd5d477454895230057439ddd
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-01 13:14:49 +0600

    feat: implement JWT authentication middleware and Cloudinary storage configuration

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -1,10 +1,11 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const env = require("./env.config");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: env.cloudinary.cloudName,
+  api_key: env.cloudinary.apiKey,
+  api_secret: env.cloudinary.apiSecret,
 });
 
 const storage = new CloudinaryStorage({
