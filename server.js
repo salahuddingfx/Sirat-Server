5c9da6a907a62839490fe9d2d5c4ab7c48ce0deb
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-03 12:18:22 +0600

    feat: implement multer configuration for image file uploads

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const env = require("./config/env.config");
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const helmet = require("helmet");
 const xss = require("./middleware/xss-clean");
