e4e9153afaa370ef414f2c4a78daf3bbe6e6eb3c
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-15 10:43:35 +0600

    feat: scaffold full server architecture with modular controllers, services, routes, and database models

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,47 @@
 const mongoose = require("mongoose");
+require("./env.config");
+
+// Register global plugin to support both UUID string IDs and legacy MongoDB ObjectIds for all schemas
+mongoose.plugin((schema) => {
+  const idPath = schema.path("_id");
+  if (idPath) {
+    const options = idPath.options || {};
+    schema.path("_id", {
+      ...options,
+      type: mongoose.Schema.Types.Mixed
+    });
+  }
+
+  const castId = (val) => {
+    if (typeof val === "string" && val.match(/^[0-9a-fA-F]{24}$/)) {
+      return new mongoose.Types.ObjectId(val);
+    }
+    return val;
+  };
+
+  const castQueryIds = (query) => {
+    if (!query) return;
+    if (query._id !== undefined) {
+      if (typeof query._id === "object" && query._id !== null) {
+        if (query._id.$in && Array.isArray(query._id.$in)) {
+          query._id.$in = query._id.$in.map(castId);
+        } else if (query._id.$nin && Array.isArray(query._id.$nin)) {
+          query._id.$nin = query._id.$nin.map(castId);
+        } else if (query._id.$ne !== undefined) {
+          query._id.$ne = castId(query._id.$ne);
+        } else if (query._id.$eq !== undefined) {
+          query._id.$eq = castId(query._id.$eq);
+        }
+      } else {
+        query._id = castId(query._id);
+      }
+    }
+  };
+
+  schema.pre(["find", "findOne", "findOneAndUpdate", "findOneAndDelete", "deleteOne", "deleteMany", "updateOne", "updateMany", "countDocuments"], function() {
+    castQueryIds(this.getQuery());
+  });
+});
 
 // Pre-register models to ensure they are loaded in mongoose
 require("../models/user.model");
